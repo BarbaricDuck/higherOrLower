b8fb1fd86045693727146a92a81cbdd2352fe8ac
bengroves2004 <52626900+bengroves2004@users.noreply.github.com>
2019-08-21 11:25:30 +0100

    added some js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var computorGuess;
+
+function init(){
+	computorGuess = Math.floor(Math.random()*100+1);
+}
+function newGame(){
+	window.location.reload(false);
+}
+         s s     sssssssssss s s s s ss sss sssssssssss       
