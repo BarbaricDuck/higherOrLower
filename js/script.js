f63d5b40a3fb88b90a1d13b31ea8b5f3bd274606
bengroves2004 <52626900+bengroves2004@users.noreply.github.com>
2020-03-14 12:01:04 +0000

    Revert "Update script.js"
    This reverts commit bef34bf31f456a2f2c0806d53b7641c9cae42bc7.

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ function changeDifficulty(){
 function gameOver(){
 	
 	gameOversfx.play();
-	alert("Game Over! The correct answer was:",answer);
+	alert("Game Over! The correct answer was:");
 	newGame();
 
 	
